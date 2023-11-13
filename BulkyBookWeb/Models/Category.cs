f332b7be87edc1116a1b6165c4e08d7d91e6c094
trandinhhieu20IT691 <trandinhhieuit@gmail.com>
2023-11-13 21:26:17 +0700

    first

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Chỉ được nhập chữ")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1 and 100 only!!")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+        
+    }
+}
